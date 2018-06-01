5663500d2f4e5af94a813a720bd03323054d6ba8
Cassandra Le <lenguyetminh1998@gmail.com>
2018-05-31 22:23:41 -0400

    GODDAMN IT, CANNOT LOAD EXTERNAL JS FILE DUE TO CSP VIOLATION

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-console.log("Hello World");
 var script = document.createElement('script');
 script.type = "text/javascript";
 url = "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js";
 
-if (script.readyState){  //IE
+if (script.readyState){
         script.onreadystatechange = function(){
             if (script.readyState == "loaded" ||
                     script.readyState == "complete"){
@@ -11,11 +10,12 @@ if (script.readyState){  //IE
                 callback();
             }
         };
-    } else {  //Others
+    }
+    else {
         script.onload = function(){
             callback();
         };
     }
 
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
+script.src = url;
+document.getElementsByTagName("head")[0].appendChild(script);
